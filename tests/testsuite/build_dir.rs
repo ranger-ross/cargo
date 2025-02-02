4bc2fe884654645e41f4574853476223dde02f4d
Ross Sullivan <rosssullivan101@gmail.com>
2025-02-02 15:46:45 +0900

    Added target-dir tests
    These ares are in preparation to split target-dir into artifact-dir and build-dir

---
+++ tests/testsuite/build_dir.rs
@@ -0,0 +1,182 @@
+//! Tests for `build.build-dir` config property.
+use std::path::PathBuf;
+
+use cargo_test_support::prelude::*;
+use cargo_test_support::project;
+
+#[cargo_test]
+fn binary_with_debug() {
+    let p = project()
+        .file("src/main.rs", r#"fn main() { println!("Hello, World!") }"#)
+        .file(
+            ".cargo/config.toml",
+            r#"
+            [build]
+            target-dir = "build"
+            "#,
+        )
+        .build();
+
+    p.cargo("build")
+        .masquerade_as_nightly_cargo(&[])
+        .enable_mac_dsym()
+        .run();
+
+    assert_build_dir(p.root().join("build"), "debug", true);
+    assert!(p.root().join("build/debug/foo").is_file());
+    assert!(!p.root().join("target").exists());
+}
+
+#[cargo_test]
+fn binary_with_release() {
+    let p = project()
+        .file("src/main.rs", r#"fn main() { println!("Hello, World!") }"#)
+        .file(
+            ".cargo/config.toml",
+            r#"
+            [build]
+            target-dir = "build"
+            "#,
+        )
+        .build();
+
+    p.cargo("build --release")
+        .masquerade_as_nightly_cargo(&[])
+        .enable_mac_dsym()
+        .run();
+
+    assert_build_dir(p.root().join("build"), "release", true);
+    assert!(p.root().join("build/release/foo").is_file());
+    assert!(!p.root().join("target").exists());
+}
+
+#[cargo_test]
+fn should_default_to_target() {
+    let p = project()
+        .file("src/main.rs", r#"fn main() { println!("Hello, World!") }"#)
+        .build();
+
+    p.cargo("build")
+        .masquerade_as_nightly_cargo(&[])
+        .enable_mac_dsym()
+        .run();
+
+    assert_build_dir(p.root().join("target"), "debug", true);
+    assert!(p.root().join("target/debug/foo").is_file());
+}
+
+#[cargo_test]
+fn cargo_doc_should_output_to_target_dir() {
+    let p = project()
+        .file("src/main.rs", r#"fn main() { println!("Hello, World!") }"#)
+        .file(
+            ".cargo/config.toml",
+            r#"
+            [build]
+            target-dir = "build"
+            "#,
+        )
+        .build();
+
+    p.cargo("doc")
+        .masquerade_as_nightly_cargo(&[])
+        .enable_mac_dsym()
+        .run();
+
+    let docs_dir = p.root().join("build/doc");
+    assert!(docs_dir.exists());
+    assert!(docs_dir.join("foo/index.html").exists());
+}
+
+#[cargo_test]
+fn cargo_package_should_output_to_target_dir() {
+    let p = project()
+        .file("src/main.rs", r#"fn main() { println!("Hello, World!") }"#)
+        .file(
+            ".cargo/config.toml",
+            r#"
+            [build]
+            target-dir = "build"
+            "#,
+        )
+        .build();
+
+    p.cargo("package")
+        .masquerade_as_nightly_cargo(&[])
+        .enable_mac_dsym()
+        .run();
+
+    assert_build_dir(p.root().join("build"), "debug", true);
+
+    let package_dir = p.root().join("build/package");
+    assert!(package_dir.exists());
+    assert!(package_dir.join("foo-0.0.1.crate").exists());
+    assert!(package_dir.join("foo-0.0.1.crate").is_file());
+    assert!(package_dir.join("foo-0.0.1").exists());
+    assert!(package_dir.join("foo-0.0.1").is_dir());
+}
+
+#[cargo_test]
+fn cargo_clean_should_clean_the_target_dir() {
+    let p = project()
+        .file("src/main.rs", r#"fn main() { println!("Hello, World!") }"#)
+        .file(
+            ".cargo/config.toml",
+            r#"
+            [build]
+            target-dir = "build"
+            "#,
+        )
+        .build();
+
+    p.cargo("build")
+        .masquerade_as_nightly_cargo(&[])
+        .enable_mac_dsym()
+        .run();
+
+    assert_build_dir(p.root().join("build"), "debug", true);
+
+    p.cargo("clean")
+        .masquerade_as_nightly_cargo(&[])
+        .enable_mac_dsym()
+        .run();
+
+    assert!(!p.root().join("build").exists());
+}
+
+#[track_caller]
+fn assert_build_dir(path: PathBuf, profile: &str, is_build_dir: bool) {
+    println!("checking if {path:?} is a build directory ({is_build_dir})");
+    // For things that are in both `target` and the build directory we only check if they are
+    // present if `is_build_dir` is true.
+    if is_build_dir {
+        assert_eq!(is_build_dir, path.join(profile).is_dir());
+    }
+
+    let error_message = |dir: &str| {
+        if is_build_dir {
+            format!("`{dir}` dir was expected but not found")
+        } else {
+            format!("`{dir}` dir was not expected but was found")
+        }
+    };
+
+    assert_eq!(
+        is_build_dir,
+        path.join(profile).join("deps").is_dir(),
+        "{}",
+        error_message("deps")
+    );
+    assert_eq!(
+        is_build_dir,
+        path.join(profile).join("build").is_dir(),
+        "{}",
+        error_message("build")
+    );
+    assert_eq!(
+        is_build_dir,
+        path.join(profile).join("incremental").is_dir(),
+        "{}",
+        error_message("incremental")
+    );
+}
